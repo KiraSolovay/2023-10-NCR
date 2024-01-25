f6b5d9f1fbc1dc1822dc0607433fc1fdca953c77
Kira Solovay <kirasolovay@gmail.com>
2024-01-25 10:43:03 -0500

    Greeting express application completed.

---
+++ software_homework/homework-12-2/server.js
@@ -0,0 +1,26 @@
+// Require modules
+const express = require('express');
+
+// Create the Express app
+const app = express();
+
+// Configure the app (app.set)
+
+
+// Mount middleware (app.use)
+
+
+// Mount routes
+app.get('/', function(req, res) {
+  	res.send('<h1>Hello World!</h1>');
+});
+
+// GREETINGS
+app.get('/greeting/:name', function(req, res) {
+    res.send(`<h1>Greetings, ${req.params.name}!<h1>`)
+})
+
+// Tell the app to listen on port 3000
+app.listen(3000, function() {
+ console.log('Listening on port 3000');
+});
