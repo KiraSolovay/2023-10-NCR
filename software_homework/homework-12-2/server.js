6e56ae45f6b786215f46f7816cee66a1b95bddb9
Kira Solovay <kirasolovay@gmail.com>
2024-01-25 10:53:06 -0500

    tip calculator express application created

--- software_homework/homework-12-2/server.js
+++ software_homework/homework-12-2/server.js
@@ -20,6 +20,12 @@ app.get('/greeting/:name', function(req, res) {
     res.send(`<h1>Greetings, ${req.params.name}!<h1>`)
 })
 
+//TIP CALCULATOR
+app.get('/tip/:total/:tipPercentage', function(req, res) {
+    const tip = (req.params.tipPercentage / 100) * req.params.total;
+    res.send(`<h1>Tip Calculator: your tip will be ${tip} </h1>`)
+})
+
 // Tell the app to listen on port 3000
 app.listen(3000, function() {
  console.log('Listening on port 3000');
